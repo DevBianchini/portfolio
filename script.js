46d0204937213e74b7726eefc8f174074ceed43e
Vinícius Bianchini <128602538+DevBianchini@users.noreply.github.com>
2024-02-15 13:06:30 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import myProjects from "./src/components/myProjects/myProjects.js";
-import headerFunction from "./src/components/header/header.js";
-import contact from "./src/components/contact/contact.js";
-
-document.addEventListener("DOMContentLoaded", myProjects);
-document.addEventListener("DOMContentLoaded", headerFunction);
-document.addEventListener("DOMContentLoaded", contact);
