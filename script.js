b531ec94b2f7c46721ebef201574ba3f36043bb5
Vinícius Bianchini <128602538+DevBianchini@users.noreply.github.com>
2024-02-15 13:08:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+import myProjects from "./myProjects.js";
+import headerFunction from "./header.js";
+import contact from "./contact.js";
+
+document.addEventListener("DOMContentLoaded", myProjects);
+document.addEventListener("DOMContentLoaded", headerFunction);
+document.addEventListener("DOMContentLoaded", contact);
